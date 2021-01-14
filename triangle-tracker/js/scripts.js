3e8fc03305cc4bc7bd3e11612ea6cd71cb67b25d
Sylvja <conneradamson@comcast.net>
2021-01-14 14:00:39 -0800

    Added styles using css for our functions
    Co-authored-by: Morgan Bradford <morganjbradford95@gmail.com>
    Co-authored-by: Jeroen van Seeters <vanseetersjeroen@gmail.com>

---
+++ triangle-tracker/js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $("form#equal").submit(function(event) {
+  
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const number3 = parseInt($("add3").val());
+
+     if (number1 === number2 && number2 === number3)
+  }); 
+}); 
+
+
+
+//2cm - 2cm - 2cm
+//equalateral
+
+//2cm - 3cm - 2cm
+//Isosceles
+
+//2cm - 3cm - 4cm
+//Scalene
+
